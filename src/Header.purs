653564f1e667ca9598c92de3585a0b548e7431e7
Peter Becich <peterbecich@gmail.com>
2017-11-12 18:10:41 -0800

    decode JSON into newtype `Stock` and display it

--- src/Header.purs
+++ src/Header.purs
@@ -2,11 +2,30 @@ module Header where
 
 import Prelude
 
+import Control.Monad.Eff
+import Control.Monad.Eff.Class
+
+import Control.Monad.Aff
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Except
+
+import Data.List.Types (NonEmptyList)
+import Data.Foreign
+import Data.Foreign.Generic
+import Data.Foreign.JSON
+
+import Data.Maybe
+import Data.Either
+import Data.Foreign (MultipleErrors)
+
+import Network.HTTP.Affjax (get, post, AJAX, AffjaxResponse)
+
 import React as R
 import React.DOM as R
 import React.DOM.Props as RP
+import Simple.JSON
 import Thermite as T
-
 import Unsafe.Coerce
 
 import Stock
@@ -19,21 +38,58 @@ import Stock
 unsafeEventValue :: forall event. event -> String
 unsafeEventValue e = (unsafeCoerce e).target.value
 
+-- https://leanpub.com/purescript/read#leanpub-auto-working-with-untyped-data
+-- type F = Except (NonEmptyList ForeignError)
+
+stockQuery :: forall e. String -> Aff (ajax :: AJAX, console :: CONSOLE | e) (Maybe Stock)
+stockQuery query = do
+  res <- get ("http://localhost:1234/tickerQuery?q="<>query)
+  liftEff $ log $ "response: " <> res.response
+  -- let
+  --   parsed :: Either MultipleErrors Stock
+  --   parsed = readJSON res.response
+  let
+    eForeign :: Either (NonEmptyList ForeignError) Foreign
+    eForeign = runExcept (parseJSON res.response)
+
+    eParsed :: Either (NonEmptyList ForeignError) Stock
+    eParsed = runExcept (decodeJSON res.response)
+  -- liftEff $ log $ case eParsed of
+  --   (Right stock) -> show stock
+  --   (Left _) -> "error"
+  pure $ hush eParsed
+    
+
 header :: T.Spec _ AppState _ AppAction
 header = T.simpleSpec performAction render
   where
     performAction :: T.PerformAction _ AppState _ AppAction
-    performAction (SubmitQuery query') _ _ =
-      void $ T.modifyState (\appState -> appState { query = query' })
+    performAction (SubmitQuery query') _ _ = do
+      mstock' <- lift $ stockQuery query'
+      void $ T.modifyState (\appState ->
+                             appState {
+                               query = query'
+                               , mstock = mstock'
+                               })
        
     render :: T.Render _ _ _
     render dispatch _ state _ =
       [ R.h1' [R.text "Header" ]
       , R.p' [ R.text "Search for a stock by name or ticker symbol" ]
-      , R.p' [ R.input [ RP.onSubmit \e -> dispatch (SubmitQuery (unsafeEventValue e)) ] []
-             , R.button [ RP.onClick \_ -> dispatch (SubmitQuery (state.query)) ] [ R.text "Submit" ]
-             ]
-      ]
+      , R.p' [
+           R.input [
+              RP.onSubmit \e ->
+               dispatch (SubmitQuery (unsafeEventValue e))
+              ] []
+           , R.button [
+              RP.onClick \_ ->
+               dispatch (SubmitQuery (state.query))
+              ] [ R.text "Submit" ]
+           ]
+      ] <> case state.mstock of
+        (Just (Stock { tickerSymbol, description })) ->
+          [ R.p' [ R.text tickerSymbol, R.text ": ", R.text description ] ]
+        Nothing -> []
 
 -- http://try.purescript.org/?gist=f5f273e4c5e4161fceff&backend=thermite&session=87ef2d8b-4fab-2175-2a20-38042f523ff7
 
--- src/Stock.purs
+++ src/Stock.purs
@@ -1,6 +1,14 @@
 module Stock where
 
+import Prelude
+
+import Data.Foreign.JSON
+import Data.Foreign.Class
+import Data.Foreign.Generic
+import Data.Generic.Rep
+
 import Data.Maybe
+import Data.Show
 
 type AppState =
   { query :: String
@@ -12,7 +20,24 @@ data AppAction = SubmitQuery String
 initialAppState =
   { query: "", mstock: Nothing }
 
-type Stock =
-  { tickerSymbol :: String }
+newtype Stock = Stock
+  { tickerSymbol :: String
+  , description :: String
+  }
 
-       
+derive instance genericStock :: Generic Stock _
+
+-- https://pursuit.purescript.org/packages/purescript-foreign-generic/4.3.0/docs/Data.Foreign.Class#t:Decode
+
+instance decodeStock :: Decode Stock where
+  decode = genericDecode (
+    defaultOptions {
+       unwrapSingleConstructors = true
+       }
+    )
+
+instance showStock :: Show Stock where
+  show (Stock { tickerSymbol, description }) =
+    tickerSymbol <> ": " <> description
+
+
