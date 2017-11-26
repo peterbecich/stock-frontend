1fdf4cf8a1d1776ae2667a16d274c31a0591f41f
Peter Becich <peterbecich@gmail.com>
2017-11-25 20:27:37 -0800

    remove host from ajax

--- src/Main.purs
+++ src/Main.purs
@@ -44,7 +44,7 @@ import StockList
 -- TODO replace with relative path?
 -- like here: https://github.com/slamdata/purescript-affjax#introduction
 --host = "http://51.15.206.218"
-host = "http://www.peterbecich.me"
+host = ""
 
 main :: forall e. Eff (ajax :: AJAX, console :: CONSOLE, dom :: DOM | e) Unit
 main = do
